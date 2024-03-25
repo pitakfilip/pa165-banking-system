d0267f235cb50ad17552f99ddb7e6955f26a957c
Martin Mojžiš <xmojzis1@fi.muni.cz>
2024-03-25 22:17:59 +0100

    fix: database

--- account-query/src/main/java/cz/muni/pa165/banking/application/repository/BalanceRepositoryImpl.java
+++ account-query/src/main/java/cz/muni/pa165/banking/application/repository/BalanceRepositoryImpl.java
@@ -1,12 +1,29 @@
 package cz.muni.pa165.banking.application.repository;
 
-import cz.muni.pa165.banking.domain.balance.repository.BalanceRepository;
+import cz.muni.pa165.banking.domain.balance.Balance;
+import cz.muni.pa165.banking.domain.balance.repository.BalancesRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Martin Mojzis
  */
 @Repository
-public class BalanceRepositoryImpl implements BalanceRepository {
-
+public class BalanceRepositoryImpl implements BalancesRepository {
+    private final Map<Integer, Balance> allBalances = new HashMap<>();
+    private Map<Integer, Balance> mockData;
+    public BalanceRepositoryImpl() {
+        mockData = Map.of(
+                1, new Balance(),
+                2, new Balance()
+        );
+    }
+    @Transactional
+    public Balance getById(Integer id) {
+        //return allBalances.get(id);
+        return allBalances.get(id);
+    }
 }
--- account-query/src/main/java/cz/muni/pa165/banking/domain/balance/repository/BalancesRepository.java
+++ account-query/src/main/java/cz/muni/pa165/banking/domain/balance/repository/BalancesRepository.java
@@ -3,5 +3,5 @@ package cz.muni.pa165.banking.domain.balance.repository;
 /**
  * @author Martin Mojzis
  */
-public interface BalanceRepository {
+public interface BalancesRepository {
 }
