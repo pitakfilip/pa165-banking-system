d7af255ba8df4246592247c267cc4f31d67b0b3c
Filip Piták <xpitak@fi.muni.cz>
2024-04-19 21:49:18 +0200

    Rename and fix mashup in DB init scripts

---
+++ .docker/db/scripts/1_create_account_management_db.sql
@@ -0,0 +1,33 @@
+SET ROLE "ACC_USER";
+SET SEARCH_PATH TO bank_user;
+
+-- CREATE TABLES
+
+CREATE TABLE IF NOT EXISTS banking_user
+(
+    id DECIMAL PRIMARY KEY,
+    email VARCHAR(50),
+    password VARCHAR(50),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    user_type VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS bank_account
+(
+    id                 DECIMAL PRIMARY KEY,
+    number             VARCHAR(50),
+    user_id            DECIMAL,
+    max_spending_limit DECIMAL,
+    type               VARCHAR(50),
+    currency           VARCHAR(50)
+);
+
+
+CREATE TABLE IF NOT EXISTS scheduled_payment
+(
+    id                  DECIMAL PRIMARY KEY,
+    sender_account_id   DECIMAL,
+    receiver_account_id DECIMAL,
+    amount              DECIMAL
+);
