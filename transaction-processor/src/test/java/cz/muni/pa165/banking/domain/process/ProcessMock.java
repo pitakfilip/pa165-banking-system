2784455e25587f7326eda2a1f2660b81f8279c66
Filip Piták <xpitak@fi.muni.cz>
2024-04-24 05:47:10 +0200

    Review tests after breaking change in Process constructor

--- transaction-processor/src/test/java/cz/muni/pa165/banking/domain/process/ProcessMock.java
+++ transaction-processor/src/test/java/cz/muni/pa165/banking/domain/process/ProcessMock.java
@@ -1,5 +1,9 @@
 package cz.muni.pa165.banking.domain.process;
 
+import cz.muni.pa165.banking.domain.process.status.Status;
+import cz.muni.pa165.banking.domain.process.status.StatusInformation;
+
+import java.time.Instant;
 import java.util.UUID;
 
 public class ProcessMock extends Process {
@@ -9,6 +13,7 @@ public class ProcessMock extends Process {
     public ProcessMock() {
         super();
         this.uuid = UUID.randomUUID();
+        this.setCurrentStatus(new StatusInformation(Instant.now(), Status.CREATED, "Process created, waiting for processing."));
     }
     
     @Override
